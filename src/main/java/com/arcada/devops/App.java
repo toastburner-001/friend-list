a3d899a1a7d72a0512b3fbc88baf1f1b63d423e9
niskanjo <niskanjo@arcada.fi>
2024-09-27 01:37:26 +0300

    Initial commit

---
+++ src/main/java/com/arcada/devops/App.java
@@ -0,0 +1,16 @@
+package com.arcada.devops;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	// Hardcoded friends list
+    	String[] friendsList = {"David Rodriguez", "Jessica Moore", "Michael Brown", "John Lopez", "Linda Anderson", "Sarah Wilson"};
+    	
+        System.out.println("Welcome to FriendGallery!");
+        
+        
+        
+    }
+}
