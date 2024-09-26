76387441732ed4d2828aab3656c16d6bb873d482
niskanjo <niskanjo@arcada.fi>
2024-09-27 02:15:28 +0300

    Friend list v0.1

--- src/main/java/com/arcada/devops/App.java
+++ src/main/java/com/arcada/devops/App.java
@@ -1,16 +1,32 @@
 package com.arcada.devops;
 
+import java.util.Scanner;
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-    	// Hardcoded friends list
-    	String[] friendsList = {"David Rodriguez", "Jessica Moore", "Michael Brown", "John Lopez", "Linda Anderson", "Sarah Wilson"};
     	
+    	Scanner input = new Scanner(System.in);
         System.out.println("Welcome to FriendGallery!");
         
+        System.out.println("Please enter a profile name.");
+        String nameInput = input.nextLine();
         
+        String[] friendList = getFriendList(nameInput);
         
+        // Output friend list
+        System.out.println("Friend list of user " + nameInput);
+        for (int i = 0; i < friendList.length; i++) {
+        	System.out.println(friendList[i]);
+        }
+       
+        
+    }
+    
+    public static String[] getFriendList(String username) {
+    	// Hardcoded friend list
+    	String[] friendList = {"David Rodriguez", "Jessica Moore", "Michael Brown", "John Lopez", "Linda Anderson", "Sarah Wilson"};
+    	return friendList;
     }
 }
